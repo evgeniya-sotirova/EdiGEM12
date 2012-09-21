a16d870a087397ec86728d0b46708d4331cfc667
Razvan Ranca <ranca.razvan@gmail.com>
2012-09-21 04:15:58 +0100

    corrected more math errors

--- averagePlot.sh
+++ averagePlot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 nr=$1
+tmp=$(echo $RANDOM)".out"
+m=${3:-""}
 
 echo "Running model $nr"
 
@@ -17,12 +19,11 @@ echo $dr, $fs, $mod, ${!mod}
 
 for i in $(seq 0 $2)
 do
-  fl=$fs$i".out"
-  q=`eval $mod`
-  echo $fl, $mod, $q
-  ../KaSim_2 -i ${!mod} -t 1.0E-3 -p 100 -o model.temp
-  cat model.temp | cut -c 2- > $fl
-  rm model.temp
+  fl=$fs$i$m".out"
+  echo $fl, $mod, ${!mod}
+  ../KaSim_2 -i ${!mod} -t 1.0E-2 -p 100 -o $tmp
+  cat $tmp | cut -c 2- > $fl
+  rm $tmp
 done
 
 java -jar ../MeanPlotViewer-v0.0.1.jar
--- plot.sh
+++ plot.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-fl='"../model_4_0.out"' 
+fls="../model_4_0.out" 
+fl='"../model_4_0.out"'
+mod1="4_MtrABC.ka"
+mod2="5_UFe.ka"
+mod3="6.ka"
 comm=""
 
 echo $0, $1, $2
 
-../KaSim_3 -i 4_MtrABC.ka -t 1.0E-3 -p 100 -o model_4_0.temp
-cat model_4_0.temp | cut -c 2- > ../model_4_0.out
+../KaSim_2 -i $mod3 -t 1.0E-2 -p 100 -o model_4_0.temp
+cat model_4_0.temp | cut -c 2- > $fls
 rm model_4_0.temp
 
-for i in 2 3 4
+rowCount=$(head -n 1 $fls | wc -w)
+echo $rowCount
+
+for i in $(seq 3 $rowCount)
 do
   comm="$comm, $fl u 1:$i w l"
 done
@@ -19,7 +26,10 @@ comm=${comm:1}
 gnuplot -persist <<TillHere
 
 set key autotitle columnhead
-set terminal png
-set output 'out.png'
 plot $comm
+
+set terminal jpeg
+set output "output.jpeg"
+replot
+
 TillHere
