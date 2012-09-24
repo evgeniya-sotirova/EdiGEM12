51771e73a26811ddef3257d2720e68cce4f09515
Razvan Ranca <ranca.razvan@gmail.com>
2012-09-24 01:23:59 +0100

    added text files and model description

--- averagePlot.sh
+++ averagePlot.sh
@@ -12,8 +12,10 @@ fs="../"$dr"/"$nr"_"
 mod4="4_MtrABC.ka"
 mod5="5_UFe.ka"
 mod6="6.ka"
+mod1="1_TCA.ka"
+mod2="2_NapC.ka"
 
-mod="mod$nr"
+#mod="mod$nr"
 
 echo $dr, $fs, $mod, ${!mod}
 
@@ -21,7 +23,7 @@ for i in $(seq 0 $2)
 do
   fl=$fs$i$m".out"
   echo $fl, $mod, ${!mod}
-  ../KaSim_2 -i ${!mod} -t 1.0E-2 -p 100 -o $tmp
+  ../KaSim_3 -i $mod1 -i $mod2 -i $mod6 -t 5.0E5 -p 1000 -o $tmp
   cat $tmp | cut -c 2- > $fl
   rm $tmp
 done
--- plot.sh
+++ plot.sh
@@ -2,21 +2,23 @@
 
 fls="../model_4_0.out" 
 fl='"../model_4_0.out"'
-mod1="4_MtrABC.ka"
-mod2="5_UFe.ka"
-mod3="6.ka"
+mod4="4_MtrABC.ka"
+mod5="5_UFe.ka"
+mod6="6.ka"
+mod1="1_TCA.ka"
+mod2="2_NapC.ka"
 comm=""
 
 echo $0, $1, $2
 
-../KaSim_2 -i $mod3 -t 1.0E-2 -p 100 -o model_4_0.temp
+../KaSim_3 -i $mod1 -i $mod2 -i $mod6 -t 5.0E5 -p 1000 -o model_4_0.temp
 cat model_4_0.temp | cut -c 2- > $fls
 rm model_4_0.temp
 
 rowCount=$(head -n 1 $fls | wc -w)
 echo $rowCount
 
-for i in $(seq 3 $rowCount)
+for i in $(seq 2 $rowCount)
 do
   comm="$comm, $fl u 1:$i w l"
 done
